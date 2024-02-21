390b9297d2e1364705e2ba485171b1d4cb5a2dfc
Daniel Carvalho Climaco <111577285+DClimaco@users.noreply.github.com>
2024-02-21 08:43:46 -0300

    Add files via upload

---
+++ Aula 20240221/ContaCorrente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20240215
+{
+    public class ContaCorrente
+    {
+        private double saldo { get; set; }
+        private string proprietario { get; set; }
+        private string log { get; set; }
+
+        public ContaCorrente(string proprietario, double saldo)
+        {
+            this.saldo = saldo;
+            this.proprietario = proprietario;
+        }
+
+        public Boolean depositar(double valor_deposito)
+        {
+            saldo += valor_deposito;
+            Console.WriteLine("\nDeposito realizado.\n");
+            return true;
+        }
+
+        public Boolean sacar(double valor_saque)
+        {
+            if(saldo >= valor_saque)
+            {
+                saldo -= valor_saque;
+                Console.WriteLine("\nDeposito realizado.\n");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Aula 20240221/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20240215
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente cont = new ContaCorrente("Daniel", 10000);
+
+            cont.depositar(1000);
+            cont.sacar(6395);
+        }
+    }
+}
